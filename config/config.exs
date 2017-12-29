e48e1bb11700d9f48290863596eaf2bab2063550
Aram Anderson <aram.j.anderson@gmail.com>
2017-12-29 14:42:36 -0700

    OAuth done

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "ddd322a6933391da7c11", #System.get_env(GITHUB_CLIENT_ID),
+  client_secret: "50752cdc34d4affdf734b7696c4aba5edb642a69" #System.get_env("GITHUB_CLIENT_SECRET")
--- web/router.ex
+++ web/router.ex
@@ -7,18 +7,21 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
+    plug Discuss.Plugs.SetUser
   end
 
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
-
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
     get "/", TopicController, :index
+    resources "/topics", TopicController
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get ":provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.
